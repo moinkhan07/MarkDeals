d6c70dcf49d4a76d186f612f62c52157b47f8e8a
Moin-6k <moin6k@gmail.com>
2023-02-01 00:01:15 +0530

    day 2 work done and added

---
+++ MarkDeals/src/main/java/com/project/controller/ProductController.java
@@ -0,0 +1,91 @@
+package com.project.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.exception.ProductException;
+import com.project.model.Product;
+import com.project.service.ProductService;
+
+
+@RestController
+public class ProductController {
+	
+	@Autowired
+	private ProductService productService;
+
+	@PostMapping("/products")
+	public ResponseEntity<Product> addProduct(@RequestBody Product product) throws ProductException{
+		Product addedProduct = productService.addProduct(product);
+		return new ResponseEntity<>(addedProduct,HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/products")
+	public ResponseEntity<Product> updateProduct(@RequestBody Product product) throws ProductException{
+		Product updatedProduct = productService.updateProduct(product);
+		return new ResponseEntity<>(updatedProduct,HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/products/{pId}")
+	public ResponseEntity<Product> deleteProduct(@PathVariable("pId") Integer productId) throws ProductException{
+		Product deletedProduct = productService.deleteProduct(productId);
+		return new ResponseEntity<>(deletedProduct,HttpStatus.OK);
+	}
+	
+	@GetMapping("/products")
+	public ResponseEntity<List<Product>> viewAllProducts() throws ProductException{
+		List<Product> allProducts = productService.viewAllProducts();
+		return new ResponseEntity<>(allProducts,HttpStatus.OK);
+	}
+	
+	@GetMapping("/productsbycategory/{category}")
+	public ResponseEntity<List<Product>> viewAllProductsByCategory(@PathVariable("category") String productCategory) throws ProductException{
+		List<Product> allProductsByCategory = productService.viewAllProductsByCategory(productCategory);
+		return new ResponseEntity<>(allProductsByCategory,HttpStatus.OK);
+	}
+	
+	@GetMapping("/productbyname/{pName}")
+	public ResponseEntity<Product> viewProductByProductName(@PathVariable("pName") String productName) throws ProductException{
+		Product productByName = productService.viewProductByProductName(productName);
+		return new ResponseEntity<>(productByName,HttpStatus.OK);
+	}
+	
+	@GetMapping("/sortinascending")
+	public ResponseEntity<List<Product>> sortProductInLowToHighPrice() throws ProductException{
+		List<Product> productsSortInAsc = productService.viewAllProductsByPriceInAscendingOrder();
+		return new ResponseEntity<>(productsSortInAsc,HttpStatus.OK);
+	}
+	
+	@GetMapping("/sortindescending")
+	public ResponseEntity<List<Product>> sortProductInHighToLowPrice() throws ProductException{
+		List<Product> productsSortInDesc = productService.viewAllProductsByPriceInDescendingOrder();
+		return new ResponseEntity<>(productsSortInDesc,HttpStatus.OK);
+	}
+	
+	@GetMapping("/productsbyrating/{productRating}")
+	public ResponseEntity<List<Product>> viewAllProductsByRating(@PathVariable("productRating") String productRating ) throws ProductException{
+		List<Product> productsByRating = productService.viewAllProductsByRating(productRating);
+		return new ResponseEntity<>(productsByRating,HttpStatus.OK);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ MarkDeals/src/main/java/com/project/service/ProductServiceImpl.java
@@ -0,0 +1,98 @@
+package com.project.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.exception.ProductException;
+import com.project.model.Product;
+import com.project.repository.ProductRepository;
+
+@Service
+public class ProductServiceImpl implements ProductService{
+	
+	@Autowired
+	private ProductRepository productRepository;
+
+	@Override
+	public Product addProduct(Product product) throws ProductException {
+		return productRepository.save(product);
+	}
+
+	@Override
+	public Product updateProduct(Product product) throws ProductException {
+		Product existingProduct = productRepository.findByName(product.getName());
+		if(existingProduct == null) {
+			throw new ProductException("Product does not exist with product name " + product.getName());
+		}
+		return productRepository.save(product);
+	}
+
+	@Override
+	public Product deleteProduct(Integer pId) throws ProductException {
+		Optional<Product> opt = productRepository.findById(pId);
+		if (opt.isPresent()) {
+			Product existingProduct = opt.get();
+			productRepository.delete(existingProduct);
+			return existingProduct;
+		}
+		throw new ProductException("Product does not exist with product Id " + pId);
+	}
+
+	@Override
+	public List<Product> viewAllProducts() throws ProductException {
+		List<Product> existingProducts = productRepository.getAllProducts();
+		if (existingProducts == null) {
+			throw new ProductException("Products does not exist");
+		}
+		return existingProducts;
+	}
+
+	@Override
+	public List<Product> viewAllProductsByCategory(String productCategory) throws ProductException {
+		List<Product> existingProductsByCategory = productRepository.findByCategory(productCategory);
+		if (existingProductsByCategory == null) {
+			throw new ProductException("Products does not exist with product category " + productCategory);
+		}
+		return existingProductsByCategory;
+	}
+
+	@Override
+	public Product viewProductByProductName(String productName) throws ProductException {
+		Product existingProduct = productRepository.findByName(productName);
+		if (existingProduct == null) {
+			throw new ProductException("Product does not exist with product name " + productName);
+		}
+		return existingProduct;
+	}
+
+	@Override
+	public List<Product> viewAllProductsByPriceInAscendingOrder() throws ProductException {
+		List<Product> ascendingProducts = productRepository.getAllProductsByPriceInAscendingOrder();
+		if (ascendingProducts == null) {
+			throw new ProductException("Products does not exist");
+		}
+		return ascendingProducts;
+	}
+
+	@Override
+	public List<Product> viewAllProductsByPriceInDescendingOrder() throws ProductException {
+		List<Product> descendingProducts = productRepository.getAllProductsByPriceInDescendingOrder();
+		if (descendingProducts == null) {
+			throw new ProductException("Products does not exist");
+		}
+		return descendingProducts;
+	}
+
+	@Override
+	public List<Product> viewAllProductsByRating(String productRating) throws ProductException {
+		List<Product> productsByRating = productRepository.findByRating(productRating);
+		if (productRating == null) {
+			throw new ProductException("Product does not exist with product rating " + productRating);
+		}
+		return productsByRating;
+	}
+
+}
