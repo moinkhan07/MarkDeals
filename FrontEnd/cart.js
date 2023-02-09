8df65ace6c088991032ee0761c0fa95421f34273
Moin-6k <moin6k@gmail.com>
2023-02-10 00:32:11 +0530

    Cart page frontend done

---
+++ FrontEnd/cart.js
@@ -0,0 +1,14 @@
+let dropdown_2 = document.querySelectorAll(".dropdown_main_");
+  dropdown_2.forEach((drop) => {
+    let select_2 = drop.querySelector(".select_");
+    let caret_2 = drop.querySelector(".caret_");
+    let menu_2 = drop.querySelector(".menu_2_");
+    // let options_2 = drop.querySelectorAll(".menu_2_ li");
+    let selected_2 = drop.querySelector(".selected_");
+    select_2.addEventListener("click",()=> {
+      select_2.classList.toggle("select_-clicked");
+      caret_2.classList.toggle("caret_-rotate");
+      menu_2.classList.toggle("menu_2_-open");
+    });
+});
+    // ===============================================
