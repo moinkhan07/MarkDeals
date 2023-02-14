bc74066a2b54c45eb3174d763e73743a57c9337c
Moin-6k <moin6k@gmail.com>
2023-02-14 18:58:06 +0530

    joincolumn annotation added in cart model class and remove column annotation

--- MarkDeals/src/main/java/com/project/model/Cart.java
+++ MarkDeals/src/main/java/com/project/model/Cart.java
@@ -9,6 +9,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,7 +26,7 @@ public class Cart {
 	private Integer cartId;
 	
 	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-	@Column(name = "userId")
+	@JoinColumn(name = "userId")
 	private List<Product> product = new ArrayList<>();
 	
 	private Double totalAmount;
