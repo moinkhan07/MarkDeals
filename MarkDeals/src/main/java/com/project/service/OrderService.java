b984c2e64d58180326bc3c58e1d4db909a78b098
Moin-6k <moin6k@gmail.com>
2023-02-20 10:15:38 +0530

    getallorders api is created and testing

--- MarkDeals/src/main/java/com/project/service/OrderService.java
+++ MarkDeals/src/main/java/com/project/service/OrderService.java
@@ -1,5 +1,7 @@
 package com.project.service;
 
+import java.util.List;
+
 import com.project.exception.OrderException;
 import com.project.model.Order;
 
@@ -7,7 +9,7 @@ public interface OrderService {
 	
 	public Order addOrder(Order order) throws OrderException;
 	
-	
+	public List<Order> getAllOrders(Integer userId) throws OrderException;
 	
 
 }
--- MarkDeals/src/main/java/com/project/service/OrderServiceImpl.java
+++ MarkDeals/src/main/java/com/project/service/OrderServiceImpl.java
@@ -1,13 +1,16 @@
 package com.project.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.project.exception.OrderException;
 import com.project.model.Order;
+import com.project.model.Users;
 import com.project.repository.OrderRepository;
+import com.project.repository.UserRepository;
 
 @Service
 public class OrderServiceImpl implements OrderService{
@@ -15,9 +18,24 @@ public class OrderServiceImpl implements OrderService{
 	@Autowired
 	private OrderRepository orderRepository;
 	
+	@Autowired
+	private UserRepository userRepository;
+	
 	@Override
 	public Order addOrder(Order order) throws OrderException {
 		return orderRepository.save(order);
+	}
+
+	@Override
+	public List<Order> getAllOrders(Integer userId) throws OrderException {
+		Optional<Users> optUser = userRepository.findById(userId);
+		if (optUser.isEmpty()) {
+			throw new OrderException("User does not exist");
+		}else {
+			Users existingUser = optUser.get();
+			List<Order> orders = existingUser.getOrders();
+			return orders;
+		}
 	}
 	
 	
